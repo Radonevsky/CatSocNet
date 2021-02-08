02f75a2f8eb777578e55b083a43a6e1ef88af72b
Radonevsky <radonevsky@yandex.ru>
2021-02-08 04:30:37 +0300

    Updates

--- precache-manifest.1e8bdc1d13b7ee054d48b684077e4642.js
+++ precache-manifest.1e8bdc1d13b7ee054d48b684077e4642.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d14402f2eb34a28b0b61a69891f548a5",
+    "revision": "5beabb3beffea67c03c27f6aa74a10fb",
     "url": "/CatSocNet/index.html"
   },
   {
@@ -8,36 +8,36 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/CatSocNet/static/css/3.bd590e61.chunk.css"
   },
   {
-    "revision": "0902779e18529d8ad052",
+    "revision": "b427631d64523cf9dc48",
     "url": "/CatSocNet/static/css/4.8f1a8f23.chunk.css"
   },
   {
-    "revision": "6ee60dc243654a8ece42",
-    "url": "/CatSocNet/static/css/main.bc8ce726.chunk.css"
+    "revision": "03c669ee95c694d40604",
+    "url": "/CatSocNet/static/css/main.0e59d01d.chunk.css"
   },
   {
-    "revision": "b22eb3ca6d3c9073819a",
-    "url": "/CatSocNet/static/js/2.d4ffe79f.chunk.js"
+    "revision": "c0505002899b5f7e0a8f",
+    "url": "/CatSocNet/static/js/2.8caa23eb.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/CatSocNet/static/js/2.d4ffe79f.chunk.js.LICENSE.txt"
+    "url": "/CatSocNet/static/js/2.8caa23eb.chunk.js.LICENSE.txt"
   },
   {
     "revision": "86fbceced1c4eed0b138",
     "url": "/CatSocNet/static/js/3.adadf038.chunk.js"
   },
   {
-    "revision": "0902779e18529d8ad052",
-    "url": "/CatSocNet/static/js/4.256e94d4.chunk.js"
+    "revision": "b427631d64523cf9dc48",
+    "url": "/CatSocNet/static/js/4.250de731.chunk.js"
   },
   {
-    "revision": "6ee60dc243654a8ece42",
-    "url": "/CatSocNet/static/js/main.9390ba1b.chunk.js"
+    "revision": "03c669ee95c694d40604",
+    "url": "/CatSocNet/static/js/main.9f35500a.chunk.js"
   },
   {
-    "revision": "7f18972f78ce2bebff52",
-    "url": "/CatSocNet/static/js/runtime-main.a75a0ddc.js"
+    "revision": "2755a3691cbff8863ec2",
+    "url": "/CatSocNet/static/js/runtime-main.bce289c2.js"
   },
   {
     "revision": "ed1a66cd713c5beef3d0e8724df7f277",
